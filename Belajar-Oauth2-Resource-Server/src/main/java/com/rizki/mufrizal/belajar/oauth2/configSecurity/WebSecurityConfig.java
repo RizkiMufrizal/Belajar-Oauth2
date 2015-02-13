75d57191e0992ea48573c428aeb39ef9a5f6ae28
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-13 18:39:23 +0700

    Add Web Security Config

---
+++ Belajar-Oauth2-Resource-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/configSecurity/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.rizki.mufrizal.belajar.oauth2.configSecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder
+                .inMemoryAuthentication();
+    }
+
+}
