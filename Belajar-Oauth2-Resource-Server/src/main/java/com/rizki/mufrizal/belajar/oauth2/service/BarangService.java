7f75a8bdb202fd0dc53eec86d7c119000b24e972
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-13 18:37:50 +0700

    Add Service Barang

---
+++ Belajar-Oauth2-Resource-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/service/BarangService.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajar.oauth2.service;
+
+import com.rizki.mufrizal.belajar.oauth2.domain.Barang;
+
+import java.util.List;
+
+public interface BarangService {
+    public void save(Barang barang);
+    public void update(Barang barang);
+    public void delete(Barang barang);
+    public Barang getBarang(String idBarang);
+    public List<Barang> getBarangList();
+}
