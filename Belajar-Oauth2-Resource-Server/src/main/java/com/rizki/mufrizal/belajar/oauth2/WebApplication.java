232615d802ffdb3a755aaee7df6c53a43922dcc8
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-13 18:36:25 +0700

    Add Web Application

---
+++ Belajar-Oauth2-Resource-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/WebApplication.java
@@ -0,0 +1,19 @@
+package com.rizki.mufrizal.belajar.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EntityScan(basePackages = {"com.rizki.mufrizal.belajar.oauth2.domain"})
+public class WebApplication {
+
+    public static void main(String[]args){
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
