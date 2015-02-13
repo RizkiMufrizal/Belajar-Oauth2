cbbdb6bad4c49237d1e565e80006dd76ed8d3a4a
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-13 18:47:35 +0700

    Add Domain User Repository

---
+++ Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajar.oauth2.repository;
+
+import com.rizki.mufrizal.belajar.oauth2.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+    @Query("select u from User u left join fetch u.userRoles pd where u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
+}
