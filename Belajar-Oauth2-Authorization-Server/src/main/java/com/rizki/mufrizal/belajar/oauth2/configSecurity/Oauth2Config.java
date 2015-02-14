2f52f1aaf43c3bfd1bdc62e20fbe62e409a8e3d5
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-14 14:53:13 +0700

    Update Oauth2 Config

--- Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/configSecurity/Oauth2Config.java
+++ Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/configSecurity/Oauth2Config.java
@@ -2,7 +2,9 @@ package com.rizki.mufrizal.belajar.oauth2.configSecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -12,9 +14,13 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
+@PropertySource("classpath:urlClient.properties")
 public class Oauth2Config {
 
     private static final String RESOURCE_ID = "BelajarOauth2";
+
+    @Value("${angularProject.url}")
+    private static String angularUrl;
 
     @Configuration
     @EnableAuthorizationServer
@@ -34,39 +40,34 @@ public class Oauth2Config {
         @Override
         public void configure(AuthorizationServerSecurityConfigurer authorizationServerSecurityConfigurer) throws Exception {
             authorizationServerSecurityConfigurer.checkTokenAccess("hasRole('CLIENT')");
+            authorizationServerSecurityConfigurer.checkTokenAccess("hasRole('USER')");
         }
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clientDetailsServiceConfigurer) throws Exception {
             clientDetailsServiceConfigurer
                     .inMemory()
-                    .withClient("clientauthcode")
-                    .secret("123456")
-                    .authorizedGrantTypes("authorization_code", "refresh_token")
-                    .scopes("read", "write")
-                    .resourceIds(RESOURCE_ID)
+                        .withClient("clientauthcode")
+                            .secret("123456")
+                            .authorizedGrantTypes("authorization_code", "refresh_token")
+                            .scopes("read", "write")
+                            .resourceIds(RESOURCE_ID)
                     .and()
-                    .withClient("clientcred")
-                    .secret("123456")
-                    .authorizedGrantTypes("client_credentials")
-                    .scopes("trust")
-                    .resourceIds(RESOURCE_ID)
+                        .withClient("clientcred")
+                            .secret("123456")
+                            .authorizedGrantTypes("client_credentials")
+                            .scopes("trust")
+                            .resourceIds(RESOURCE_ID)
                     .and()
-                    .withClient("clientapp")
-                    .secret("123456")
-                    .authorizedGrantTypes("password")
-                    .scopes("read", "write")
-                    .resourceIds(RESOURCE_ID)
-                    .and()
-                    .withClient("angularClient")
-                    .secret("angularPassword")
-                    .authorizedGrantTypes("implicit")
-                    .scopes("read", "write")
-                    .resourceIds(RESOURCE_ID)
-                    .authorities("CLIENT")
-                    .redirectUris("http://localhost:20000/implicit-client/")
-                    .accessTokenValiditySeconds(60 * 60 * 24) // token berlaku seharian, besok harus login ulang
-                    .autoApprove(true);
+                        .withClient("angularClient")
+                            .secret("angularPassword")
+                            .authorizedGrantTypes("implicit")
+                            .scopes("read", "write")
+                            .resourceIds(RESOURCE_ID)
+                            .authorities("CLIENT", "USER")
+                            .redirectUris(angularUrl)
+                            .accessTokenValiditySeconds(60 * 60 * 24) //untuk sehari
+                            .autoApprove(true);
         }
 
     }
