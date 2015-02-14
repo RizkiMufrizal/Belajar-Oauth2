f48422fefabfb4904bf708599d869b5d00feaf0f
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-14 11:10:13 +0700

    Update User Detail

--- Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/service/UserDetailLoginService.java
+++ Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/service/UserDetailLoginService.java
@@ -12,9 +12,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailLoginService implements UserDetailsService {
@@ -35,15 +35,11 @@ public class UserDetailLoginService implements UserDetailsService {
     }
 
     private User buildUserForAuthentication(com.rizki.mufrizal.belajar.oauth2.domain.User user, List<GrantedAuthority> authorities) {
-        return new User(user.getUsername(), user.getPassword(), user.isEnable(), true, true, true, authorities);
+        return new User(user.getEmail(), user.getPassword(), user.isEnable(), true, true, true, authorities);
     }
 
     private List<GrantedAuthority> buildUserAuthority(Set<UserRole> userRoleSet) {
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-
-        for (UserRole userRole : userRoleSet) {
-            grantedAuthorities.add(new SimpleGrantedAuthority(userRole.getRole()));
-        }
+        Set<GrantedAuthority> grantedAuthorities = userRoleSet.stream().map(userRole -> new SimpleGrantedAuthority(userRole.getRole())).collect(Collectors.toSet());
 
         return new ArrayList<>(grantedAuthorities);
     }
