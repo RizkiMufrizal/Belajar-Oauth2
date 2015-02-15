00499619ee3fb5d961f6023063699806f4915035
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-15 17:59:07 +0700

    Add Library

--- Belajar-Oauth2-Implicit-Client/src/main/webapp/test/karma.conf.js
+++ Belajar-Oauth2-Implicit-Client/src/main/webapp/test/karma.conf.js
@@ -30,6 +30,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/bootbox/bootbox.js',
+      'bower_components/ngBootbox/dist/ngBootbox.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
