b2a4e34644f8b8aec0872f8213ed64c4eefb92f6
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-16 23:58:51 +0700

    Add Cache User

--- Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/repository/UserRepository.java
+++ Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.rizki.mufrizal.belajar.oauth2.repository;
 
 import com.rizki.mufrizal.belajar.oauth2.domain.User;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    @Cacheable(value = "user")
     @Query("select u from User u left join fetch u.userRoles pd where u.email = :email")
     public User getUserByUsername(@Param("email") String email);
 }
