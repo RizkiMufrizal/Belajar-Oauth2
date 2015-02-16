30c5b1b08604a6c4e5dd64ab298558a6d4465bec
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-16 23:58:32 +0700

    Add Config Redis Cache

--- Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/WebApplication.java
+++ Belajar-Oauth2-Authorization-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/WebApplication.java
@@ -3,12 +3,21 @@ package com.rizki.mufrizal.belajar.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
+@EnableCaching
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EntityScan(basePackages = {"com.rizki.mufrizal.belajar.oauth2.domain"})
@@ -21,8 +30,31 @@ public class WebApplication extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
         resourceHandlerRegistry.addResourceHandler("/**").addResourceLocations("/");
-        //resourceHandlerRegistry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
-        //resourceHandlerRegistry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName("localhost");
+        connectionFactory.setPort(6379);
+        connectionFactory.setUsePool(true);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(){
+        RedisTemplate<Object, Object> objectRedisTemplate = new RedisTemplate<>();
+        objectRedisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return objectRedisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(){
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate());
+        Map<String, Long> keyMap = new HashMap<>();
+        keyMap.put("user", 100L);
+        cacheManager.setExpires(keyMap);
+        return cacheManager;
     }
 
 }
