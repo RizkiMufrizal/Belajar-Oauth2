8ea3eb63d84ff43934b3ad25b0ecb9dd7db42fd6
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-16 23:59:08 +0700

    Add Cache Barang

--- Belajar-Oauth2-Resource-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/service/impl/BarangServiceImpl.java
+++ Belajar-Oauth2-Resource-Server/src/main/java/com/rizki/mufrizal/belajar/oauth2/service/impl/BarangServiceImpl.java
@@ -3,6 +3,8 @@ package com.rizki.mufrizal.belajar.oauth2.service.impl;
 import com.rizki.mufrizal.belajar.oauth2.domain.Barang;
 import com.rizki.mufrizal.belajar.oauth2.repository.BarangRepository;
 import com.rizki.mufrizal.belajar.oauth2.service.BarangService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,28 +19,33 @@ public class BarangServiceImpl implements BarangService {
     private BarangRepository barangRepository;
 
     @Transactional(readOnly = false)
+    @CacheEvict(value = "barang", allEntries = true)
     @Override
     public void save(Barang barang) {
         barangRepository.save(barang);
     }
 
     @Transactional(readOnly = false)
+    @CacheEvict(value = "barang", allEntries = true)
     @Override
     public void update(Barang barang) {
         barangRepository.saveAndFlush(barang);
     }
 
     @Transactional(readOnly = false)
+    @CacheEvict(value = "barang", allEntries = true)
     @Override
     public void delete(Barang barang) {
         barangRepository.delete(barang);
     }
 
+    @Cacheable(value = "barang", key = "#idBarang")
     @Override
     public Barang getBarang(String idBarang) {
         return barangRepository.findOne(idBarang);
     }
 
+    @Cacheable(value = "barang")
     @Override
     public List<Barang> getBarangList() {
         return barangRepository.findAll();
