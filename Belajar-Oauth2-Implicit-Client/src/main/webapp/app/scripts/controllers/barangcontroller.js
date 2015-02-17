e0667ba6cd312933af3efb4533b8dd9859f8aff9
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-17 08:08:38 +0700

    Update Barang Controller

--- Belajar-Oauth2-Implicit-Client/src/main/webapp/app/scripts/controllers/barangcontroller.js
+++ Belajar-Oauth2-Implicit-Client/src/main/webapp/app/scripts/controllers/barangcontroller.js
@@ -16,7 +16,19 @@
 
     var barang = this;
 
-    barang.coba = 'hello word';
+    barang.inputanBarang = {};
+    barang.enable = true;
+    barang.dataBarang = {};
+
+    barang.getBarang = function() {
+      if (!$window.sessionStorage.getItem('token')) {
+        console.log('token belum ada, silahkan login dulu');
+      }
+
+      BarangFactory.getAllBarang($window.sessionStorage.getItem('token')).query({}, function(data) {
+        barang.dataBarang = data;
+      });
+    };
 
     function refreshBarang() {
 
@@ -30,6 +42,55 @@
     }
 
     refreshBarang();
+
+    barang.saveBarang = function(b) {
+      BarangFactory.saveBarang($window.sessionStorage.getItem('token')).query({}, b).$promise.then(function() {
+        refreshBarang();
+      });
+    };
+
+    barang.clear = function() {
+      barang.inputanBarang.idBarang = '';
+      barang.inputanBarang.namaBarang = '';
+      barang.inputanBarang.jenisBarang = '';
+      barang.inputanBarang.tanggalKadaluarsa = '';
+      barang.enable = false;
+    };
+
+    barang.editBarang = function(b) {
+      barang.inputanBarang.idBarang = b.idBarang;
+      barang.inputanBarang.namaBarang = b.namaBarang;
+      barang.inputanBarang.jenisBarang = b.jenisBarang;
+      barang.inputanBarang.tanggalKadaluarsa = b.tanggalKadaluarsa;
+      barang.enable = true;
+    };
+
+    barang.updateBarang = function(b) {
+      BarangFactory.updateBarang($window.sessionStorage.getItem('token')).query({}, b).$promise.then(function() {
+        refreshBarang();
+      });
+    };
+
+    barang.deleteBarang = function(b) {
+      BarangFactory.deleteBarang(b.idBarang, $window.sessionStorage.getItem('token')).query({}).$promise.then(function() {
+        refreshBarang();
+      });
+    };
+
+    barang.jenisBarangCombo = [
+      {
+        'jenis': 'Cair',
+        'value': 'CAIR'
+      },
+      {
+        'jenis': 'Gas',
+        'value': 'GAS'
+      },
+      {
+        'jenis': 'Padat',
+        'value': 'PADAT'
+      }
+    ];
 
   }
 
